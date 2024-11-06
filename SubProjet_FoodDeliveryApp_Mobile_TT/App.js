3e6b46065c81881dc4a223cf6015504b1c1d0769
Khắc Thắng Bùi <buikhacthang910.42@gmail.com>
2024-11-06 22:22:09 +0700

    Thêm các file cần thiết

--- SubProjet_FoodDeliveryApp_Mobile_TT/App.js
+++ SubProjet_FoodDeliveryApp_Mobile_TT/App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import "nativewind";
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className="text-red-500">Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
