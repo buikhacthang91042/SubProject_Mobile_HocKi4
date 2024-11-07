7f3b32cbead280b7d8ed27dd4342aa3b97320167
Khắc Thắng Bùi <buikhacthang910.42@gmail.com>
2024-11-07 11:45:06 +0700

    Done Navigation

--- SubProjet_FoodDeliveryApp_Mobile_TT/App.js
+++ SubProjet_FoodDeliveryApp_Mobile_TT/App.js
@@ -1,13 +1,13 @@
+import { enableScreens } from 'react-native-screens';
+enableScreens();
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
+import Navigation from './navigation';
+
 export default function App() {
   return (
-    <View style={tw`flex-1 bg-white items-center justify-center`}>
-      <Text style={tw`text-red-500 text-lg`}>Hello</Text>
-      <Text style={tw`text-3xl font-bold underline`}>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation />
   );
 }
