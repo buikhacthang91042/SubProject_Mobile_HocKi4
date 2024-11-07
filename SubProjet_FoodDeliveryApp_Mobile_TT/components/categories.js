f86dac4a5879b410843c70e4bdcae50323fcffb5
Khắc Thắng Bùi <buikhacthang910.42@gmail.com>
2024-11-07 14:33:36 +0700

    Done Categories and Featured

---
+++ SubProjet_FoodDeliveryApp_Mobile_TT/components/categories.js
@@ -0,0 +1,44 @@
+import { View, Text, ScrollView, TouchableOpacity, Image,StyleSheet  } from 'react-native'
+import React, { useState } from 'react'
+import tw from 'tailwind-react-native-classnames'
+import {categories} from '../constants'
+export default function Categories() {
+    const [selectedCategory, setSelectedCategory] = useState(null);
+  return (
+    <View style={tw`mr-4`}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}
+            style={tw`overflow-visible`}
+            contentContainerStyle={{
+                paddingHorizontal:15
+            }}
+        > 
+            {
+                categories.map((category, index) => {
+                    let chon = category.id==selectedCategory;
+                    let doiMauNut = chon ? tw`bg-gray-600` : tw`bg-gray-200`;
+                    let doiMauChu = chon ? tw`font-semibold text-gray-800` : tw`text-gray-500`;
+                    return(
+                        <View key={index} style={tw`flex justify-center items-center mr-6`}>
+                            <TouchableOpacity 
+                                style={[tw`p-1 rounded-full shadow bg-gray-200`, doiMauNut]}
+                                onPress={()=> setSelectedCategory(category.id)}
+                            >
+                                <Image source={category.image} style={styles.categories_image}></Image>
+                                
+                            </TouchableOpacity>
+                            <Text style={[tw`text-sm`, doiMauChu]}>{category.name}</Text>
+                        </View>
+                    )
+                })
+            }
+        </ScrollView>
+    </View>
+  )
+}
+
+const styles= StyleSheet.create({
+    categories_image: {
+        width:45,
+        height:45
+    }
+})
--- SubProjet_FoodDeliveryApp_Mobile_TT/screens/HomeScreen.js
+++ SubProjet_FoodDeliveryApp_Mobile_TT/screens/HomeScreen.js
@@ -1,11 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, TextInput } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-react-native-classnames";
+import { StatusBar } from "expo-status-bar";
+import * as Icon from "react-native-feather";
+import { themeColors } from "../theme";
+import { ScrollView } from "react-native";
+import Categories from "../components/categories";
+import { featured } from "../constants";
+import FeaturedRow from "../components/featuredRow";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white`}>
+      <StatusBar barStyle="dark-content" />
+
+      {/* Thanh tìm kiếm */}
+      <View style={tw`flex-row items-center px-4 pb-2`}>
+        <View
+          style={tw`flex-row flex-1 items-center p-3 rounded-full border border-gray-300`}
+        >
+          <Icon.Search height="25" width="25" stroke="gray" />
+          <TextInput placeholder="Restaurants" style={tw`ml-2 flex-1`} />
+          <View
+            style={[
+              tw`flex-row items-center`,
+              {
+                borderLeftWidth: 2,
+                borderLeftColor: "#D1D5DB",
+                paddingLeft: 8,
+              },
+            ]}
+          >
+            <Icon.MapPin height="20" width="20" stroke="gray" />
+            <Text style={[tw`text-gray-600`, { marginLeft: 10 }]}>
+              Gò Vấp, HCM
+            </Text>
+          </View>
+        </View>
+        <View
+          style={[
+            tw`p-3 rounded-full`,
+            { marginLeft: 10, backgroundColor: themeColors.bgColor(1) },
+          ]}
+        >
+          <Icon.Sliders
+            height="20"
+            width="20"
+            strokeWidth={2.5}
+            stroke="white"
+          />
+        </View>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 20,
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+          <Categories />
+        
+        <View style={tw`mt-5`}>
+          {
+            featured.map((item,index)=> {
+              return(
+                <FeaturedRow 
+                key={index}
+                title= {item.title}
+                description = {item.description}
+                restaurants= {item.restaurants}
+                
+                
+                />
+              )
+            })
+          }
+        </View>
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
