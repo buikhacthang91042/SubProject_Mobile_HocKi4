fd42922b8408d949cc12e9a8d11ed2cef6be4606
LAPTOP-23NHUF9Q\home <buikhacthang910.42@gmail.com>
2024-11-09 09:37:42 +0700

    Fix loi 2 git

--- SubProjet_FoodDeliveryApp_Mobile_TT/navigation.js
+++ SubProjet_FoodDeliveryApp_Mobile_TT/navigation.js
@@ -5,6 +5,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
+import Login from './screens/Login';
 
 export default function Navigation() {
   return (
@@ -12,6 +13,7 @@ export default function Navigation() {
       <Stack.Navigator screenOptions={{
         headerShown:false
       }}>
+      <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Restaurant" component={RestaurantScreen} />
       </Stack.Navigator>
